a5623badb3d097bf9cec1ad4ccc5e85cc157933e
James King <j@lostwith.me>
2018-05-06 17:16:16 +0100

    add data into build task

--- gulpfile.js
+++ gulpfile.js
@@ -73,6 +73,7 @@ gulp.task('pug', () => {
 gulp.task('html', ['styles', 'scripts'], () => {
   return gulp
     .src('app/*.pug')
+    .pipe($.data(file => ({ data: require('./app/data.json') })))
     .pipe($.useref({ searchPath: ['.tmp', 'app', '.'] }))
     .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
     .pipe($.if(/\.css$/, $.cssnano({ safe: true, autoprefixer: false })))
